bc09dc3bfa65a43599acaf268570c14c5e94bde7
ashucAK <singhashishk12@gmail.com>
2024-03-10 05:41:37 +0530

    first commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <header class="bg-blue-500 text-white py-4 flex justify-between items-center px-4">
+    <div class="flex items-center">
+      <h1 class="text-lg font-semibold hover:text-gray-300 hover:shadow-md">Hello Airtribe</h1>
+      <img src="https://imgs.search.brave.com/PkI7xjDmPp02Q3Ywtt37Py5UK5K8R0_uJl650jtbaDA/rs:fit:860:0:0/g:ce/aHR0cHM6Ly96ZW5w/cm9zcGVjdC1wcm9k/dWN0aW9uLnMzLmFt/YXpvbmF3cy5jb20v/dXBsb2Fkcy9waWN0/dXJlcy82NWQ4MzA1/YjU0ZDE4YzAwMDEz/YTMzOGMvcGljdHVy/ZQ" alt="Airtribe Logo" class="w-10 h-10 ml-4">
+    </div>
+    <button></button>
+  </header>
+  <Html class="bg-blue-500">
+    <div>
+      <NuxtPage />
+    </div>
+    <UNotifications />
+  </Html>
+</template>
---
+++ components/BoardColumn.vue
@@ -0,0 +1,137 @@
+<script setup>
+import { ref } from 'vue'
+import { useBoardStore } from '../stores/boardStore'
+import { useRouter } from 'vue-router'
+
+const props = defineProps({
+  column: {
+    type: Object,
+    required: true
+  },
+  columnIndex: {
+    type: Number,
+    required: true
+  }
+})
+
+const boardStore = useBoardStore()
+const router = useRouter()
+
+const editNameState = ref(false)
+const newTaskName = ref('')
+const showTaskModal = ref(false)
+
+function addTask() {
+  boardStore.addTask({
+    taskName: newTaskName.value,
+    columnIndex: props.columnIndex
+  })
+  newTaskName.value = ''
+  showTaskModal.value = false // Close the modal after adding the task
+}
+
+function editColumn() {
+  editNameState.value = !editNameState.value
+}
+
+function dropItem(event, { toColumnIndex, toTaskIndex }) {
+  const type = event.dataTransfer.getData('type')
+  const fromColumnIndex = event.dataTransfer.getData('from-column-index')
+
+  if (type === 'task') {
+    const fromTaskIndex = event.dataTransfer.getData('from-task-index')
+
+    boardStore.moveTask({
+      fromTaskIndex,
+      toTaskIndex,
+      fromColumnIndex,
+      toColumnIndex
+    })
+  } else if (type === 'column') {
+    boardStore.moveColumn({
+      fromColumnIndex,
+      toColumnIndex
+    })
+  }
+}
+
+function goToTask(taskId) {
+  router.push(`/tasks/${taskId}`)
+}
+
+function pickupColumn(event, fromColumnIndex) {
+  event.dataTransfer.effectAllowed = 'move'
+  event.dataTransfer.dropEffect = 'move'
+  event.dataTransfer.setData('type', 'column')
+  event.dataTransfer.setData('from-column-index', fromColumnIndex)
+}
+
+function pickupTask(event, { fromColumnIndex, fromTaskIndex }) {
+  event.dataTransfer.effectAllowed = 'move'
+  event.dataTransfer.dropEffect = 'move'
+  event.dataTransfer.setData('type', 'task')
+  event.dataTransfer.setData('from-column-index', fromColumnIndex)
+  event.dataTransfer.setData('from-task-index', fromTaskIndex)
+}
+</script>
+
+<template>
+  <UContainer
+    class="column"
+    draggable="true"
+    @dragstart.self="pickupColumn($event, columnIndex)"
+    @dragenter.prevent
+    @dragover.prevent
+    @drop.stop="dropItem($event, { toColumnIndex: columnIndex })"
+  >
+    <div class="column-header mb-4">
+      <div>
+        <UInput v-if="editNameState" type="text" v-model="column.name" />
+        <h2 v-else>{{ column.name }}</h2>
+        <p>{{ column.tasks.length }} tasks</p>
+      </div>
+      <div>
+        <UButton class="bg-blue-500 hover:bg-blue-600 text-white" @click="editColumn">Edit</UButton>
+      </div>
+    </div>
+    <ul>
+      <li v-for="(task, taskIndex) in column.tasks" :key="task.id">
+        <UCard
+          class="mb-4"
+          @click="goToTask(task.id)"
+          draggable="true"
+          @dragstart="
+            pickupTask($event, {
+              fromColumnIndex: columnIndex,
+              fromTaskIndex: taskIndex
+            })
+          "
+          @drop.stop="
+            dropItem($event, {
+              toColumnIndex: columnIndex,
+              toTaskIndex: taskIndex
+            })
+          "
+        >
+          <strong>{{ task.name }}</strong>
+          <p>{{ task.description }}</p>
+        </UCard>
+      </li>
+    </ul>
+    <!-- Task creation modal -->
+    <UModal v-model="showTaskModal" @close="showTaskModal = false">
+      <div>
+        <h3>Create New Task</h3>
+        <UInput v-model="newTaskName" placeholder="Task name" />
+        <UButton @click="addTask">Add Task</UButton>
+      </div>
+    </UModal>
+    <!-- New Task Button -->
+    <UButton
+      icon="i-heroicons-plus-circle-solid"
+      @click="showTaskModal = true"
+    >
+      New
+    </UButton>
+  </UContainer>
+</template>
